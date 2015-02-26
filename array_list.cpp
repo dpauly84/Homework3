1299f31d0918aa533957e87c4b351b3531749492
Derek <dpauly84@gmail.com>
2015-02-26 11:25:59 -0600

    from laptop

---
+++ array_list.cpp
@@ -0,0 +1,29 @@
+// File Name: array_list.cpp
+// Author: Derek Pauly
+// Student ID: s829f376
+// Assignment Number: 3
+// Last Changed: February 26, 2015
+
+#include "array_list.hpp"
+using namespace std;
+
+Partlist::Partlist() {
+}
+
+int Partlist::end() {
+}
+
+int Partlist::next(int current_pos) {
+}
+
+bool Partlist::is_full() {
+}
+
+Part Partlist::retrieve(int position) {
+}
+
+void Partlist::remove(int position) {
+}
+
+void Partlist::add(Part newpart) {
+}
---
+++ array_list.hpp
@@ -0,0 +1,40 @@
+// File Name: array_list.hpp
+// Author: Derek Pauly
+// Student ID: s829f376
+// Assignment Number: 3
+// Last Changed: February 26, 2015
+
+#ifndef _ARRAY_LIST_HPP
+#define _ARRAY_LIST_HPP
+
+
+
+const int MAX_PARTS = 100;
+
+struct Part // TODO
+{
+    std::string pname; // part name
+    std::string pnumber; // part number format DDDD-DD where D is a digit
+    double weight; // part weight
+    std::string supplier1; // part suppllier1 format CDDD where C is a character
+    std::string supplier2; // part supplier2 format CDDD
+    int inStock; // Quantity of parts in stock
+};
+
+class Partlist
+{
+public:
+    Partlist();
+    int first();
+    int end();
+    int next(int current_pos);
+    bool is_full();
+    Part retrieve(int position);
+    void remove(int position);
+    void add(Part newpart);
+private:
+    Part m_elements[MAX_PARTS];
+    int m_last;
+};
+
+#endif
